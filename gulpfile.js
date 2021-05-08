79532eafcadf30db91aee2bab0f7b96fd95d80c7
Alexis Gantz <argantz@lcmail.lcsc.edu>
2021-05-07 17:08:17 -0700

    big update
    fixed navigation for mobile, put times in a table so it looked better on mobile, added mascot drawing.

--- gulpfile.js
+++ gulpfile.js
@@ -30,33 +30,3 @@ gulp.task('serve', gulp.series('sass', function() {
 }));
 
 gulp.task('default', gulp.series('js', 'serve')); //adding js
-
-
-const { watch, series, parallel, src, dest, gulp } = require('gulp');
-const connect = require('gulp-connect'); // Runs a local webserver
-const open = require('gulp-open'); // Opens a URL in a web browser
-
-// Launch Chrome web browser
-// https://www.npmjs.com/package/gulp-open
-function openBrowser(done) {
-    var options = {
-    uri: 'http://localhost:8080'
-    };
-    return src('./')
-    .pipe(open(options));
-    done();
-}
-
-// Gulp plugin to run a webserver (with LiveReload)
-// https://www.npmjs.com/package/gulp-connect
-function server(done) {
-    return connect.server({
-    root: './',
-    port: 8080,
-    debug: true,
-    });
-    done();
-}
-
-// Default Gulp command
-exports.default = series(openBrowser, server);
